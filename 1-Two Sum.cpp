d1bd2ae6534ef0cc0417af43856b07e4175a6606
parinovK <62334658+parinovK@users.noreply.github.com>
2023-09-07 16:57:11 +0300

    Add files via upload

---
+++ 1-Two Sum.cpp
@@ -0,0 +1,48 @@
+﻿// deci-binary.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+/*
+
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+	vector<int> digits;
+	string input;
+	string token;
+	cout << "digits: ";
+	getline(cin, input);
+	int target;
+	cout << "target: ";
+	cin >> target;
+	stringstream ss(input);
+	char separator = ' ';
+	while (getline(ss, token, separator)) {
+		digits.push_back(stoi(token));
+	}
+
+	//находим сумму
+	int temp[2];
+	bool status = false;
+	for (int i = 0; i < digits.size(); i++) {
+		for (int j = digits.size() - 1; j >= 0; j--) {
+			if (digits[i] + digits[j] == target) {
+				status = true;
+				temp[1] = i;
+				temp[0] = j;
+			}
+		}
+	}
+	
+	if (status) {
+		cout << "[" << temp[0] << "," << temp[1] << "]";
+	}
+	else {
+		cout << "digits is not found.";
+	}
+}
