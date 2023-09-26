324c65386b14b2f079c465b5ac5e9b450cae3c01
parinovK <62334658+parinovK@users.noreply.github.com>
2023-09-26 15:51:52 +0300

    Solution for problem #9

---
+++ Number 9 - Palindrome number.cpp
@@ -0,0 +1,64 @@
+﻿// atn.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+bool isPalindrome(int x) {
+    if (x < 0) {
+        return false;
+    }
+    stringstream test;
+    while (x % 10 != 0) {
+        test << x % 10;
+        x /= 10;
+    }
+    string temp = test.str();
+    reverse(temp.begin(), temp.end());
+    if (temp[0] == 0) {
+        return false;
+    }
+    if (test.str() == temp) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int main()
+{
+    cout << isPalindrome(121);
+}
+
+/*
+Решение для LeetCode:
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        stringstream test;
+        while (x % 10 != 0) {
+            test << x % 10;
+            x /= 10;
+        }
+        string temp = test.str();
+        reverse(temp.begin(), temp.end());
+        if (temp[0] == 0) {
+             return false;
+        }
+        if (test.str() == temp) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+*/
